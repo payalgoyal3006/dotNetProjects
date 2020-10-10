c4399bb873d1cb02a4bca81910fc4f810639280f
Payal Goyal <payalgoyal3006@gmail.com>
2020-10-10 22:25:27 +0530

    working_demo_semantic

---
+++ semantic_analysis/Controllers/PredictionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML.Data;
+using SemanticML.Model;
+
+
+namespace semantic.Controllers
+{
+    public class PredictionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Prediction()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Prediction(ModelInput input)
+        {
+            ViewBag.Result = "";
+            var predictions = ConsumeModel.Predict(input);
+            ViewBag.Result = predictions;
+
+            ViewData["Feedbacktext"] = input.Feedbacktext;
+
+            return View();
+        }
+    }
+}
