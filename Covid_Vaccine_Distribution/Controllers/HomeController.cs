a1d3038067cae987b23fcf6ef5abeedfad580aa9
Payal Goyal <payalgoyal3006@gmail.com>
2020-10-26 13:05:12 +0530

    covid project

---
+++ Covid_Vaccine_Distribution/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML.Data;
+using Covid_Vaccine_DistributionML.Model;
+
+namespace Covid_Vaccine_Distribution.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(ModelInput input)
+        {
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            ViewBag.Vaccine = result.Score;
+            return View(input);
+        }
+    }
+}
